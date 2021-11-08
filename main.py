3375c5d3b1e6fc1678c138708abf0bcbb936d371
Cam-g-911 <93203511+Cam-g-911@users.noreply.github.com>
2021-11-08 10:17:32 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.py
@@ -0,0 +1,48 @@
+distance = 0
+
+def on_forever():
+    global distance
+    distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
+    if distance <= 5:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        # # # # #
+        """)
+    elif distance <= 10:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        # # # # #
+                        # # # # #
+        """)
+    elif distance <= 15:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+    elif distance <= 20:
+        basic.show_leds("""
+            . . . . .
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+    elif distance <= 25:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+    else:
+        basic.show_icon(IconNames.NO)
+basic.forever(on_forever)
